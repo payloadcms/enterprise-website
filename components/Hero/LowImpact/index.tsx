af2c25c35a1dfbeda344fb89ff593ba6a85a014c
Jarrod Flesch <jarrodmflesch@gmail.com>
2022-10-25 11:35:19 -0400

    fix: large-body serializing, simplifies header margins, adjusts hero cols

--- components/Hero/LowImpact/index.tsx
+++ components/Hero/LowImpact/index.tsx
@@ -3,13 +3,18 @@ import { Cell, Grid } from '@faceless-ui/css-grid';
 import { Page } from '../../../payload-types';
 import { Gutter } from '../../Gutter';
 import RichText from '../../RichText';
+import { VerticalPadding } from '../../VerticalPadding';
+
+import classes from './index.module.scss'
 
 export const LowImpactHero: React.FC<Page['hero']> = ({ richText }) => {
   return (
     <Gutter>
       <Grid>
-        <Cell cols={8}>
-          <RichText content={richText} />
+        <Cell cols={8} colsL={10}>
+          <VerticalPadding>
+            <RichText className={classes.richText} content={richText} />
+          </VerticalPadding>
         </Cell>
       </Grid>
     </Gutter>
