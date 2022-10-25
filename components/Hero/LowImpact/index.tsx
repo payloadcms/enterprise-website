d0b540c32339a039789c66ae50c73801527667c8
Jarrod Flesch <jarrodmflesch@gmail.com>
2022-10-25 11:04:07 -0400

    feat: adds grid to hero

--- components/Hero/LowImpact/index.tsx
+++ components/Hero/LowImpact/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Cell, Grid } from '@faceless-ui/css-grid';
 import { HeroProps } from '..';
 import { Gutter } from '../../Gutter';
 import { Label } from '../../Label';
 import RichText from '../../RichText';
 
-import classes from './index.module.scss';
-
 export const LowImpactHero: React.FC<HeroProps> = ({ richText, media, title }) => {
   return (
     <Gutter>
-      <Label>{title}</Label>
-      <RichText content={richText} />
+      <Grid>
+        <Cell cols={8}>
+          <Label>{title}</Label>
+          <RichText content={richText} />
+        </Cell>
+      </Grid>
     </Gutter>
   )
 }
