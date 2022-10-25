2f4743f16201100ad7f6e6c35d7ad82fa4d403b8
Jarrod Flesch <jarrodmflesch@gmail.com>
2022-10-25 11:06:40 -0400

    feat: style h1 as h2 specifically on low impact hero

--- components/Hero/LowImpact/index.tsx
+++ components/Hero/LowImpact/index.tsx
@@ -5,13 +5,15 @@ import { Gutter } from '../../Gutter';
 import { Label } from '../../Label';
 import RichText from '../../RichText';
 
+import classes from './index.module.scss';
+
 export const LowImpactHero: React.FC<HeroProps> = ({ richText, media, title }) => {
   return (
     <Gutter>
       <Grid>
         <Cell cols={8}>
           <Label>{title}</Label>
-          <RichText content={richText} />
+          <RichText className={classes.richText} content={richText} />
         </Cell>
       </Grid>
     </Gutter>
