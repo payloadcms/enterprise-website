df839b09d437b815c7b0c6fda3a38fd7e9ca18e7
Jarrod Flesch <jarrodmflesch@gmail.com>
2022-10-25 10:59:34 -0400

    feat: renders and styles low impact hero

--- components/Hero/LowImpact/index.tsx
+++ components/Hero/LowImpact/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { HeroProps } from '..';
+import { Gutter } from '../../Gutter';
+import { Label } from '../../Label';
+import RichText from '../../RichText';
 
 import classes from './index.module.scss';
 
 export const LowImpactHero: React.FC<HeroProps> = ({ richText, media, title }) => {
   return (
-    <div className={classes.hero}>
-      low impact
-    </div>
+    <Gutter>
+      <Label>{title}</Label>
+      <RichText content={richText} />
+    </Gutter>
   )
 }
